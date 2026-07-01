d6d4aceecc5621c78b52cb735deb9922a38229fc
codeTukai <tukaighosh753@gmail.com>
2026-07-01 21:04:34 +0530

    fix: update CORS configuration

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://admin-repo-olive.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
